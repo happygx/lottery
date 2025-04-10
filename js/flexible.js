784ca5f35b0e9c875d947ebcf2c8192c58ca7e72
happygx <463275576@qq.com>
2025-04-10 21:09:54 +0800

    初始提交

---
+++ js/flexible.js
@@ -0,0 +1,19 @@
+//rem size
+var fun = function(doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if (clientWidth >= 750) {
+                docEl.style.fontSize = 100 + 'px';
+            } else {
+                docEl.style.fontSize = (clientWidth / 750) * 100 + 'px';
+
+            }
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+};
+fun(document, window);
